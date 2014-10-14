3d0e2fb28e63ff824c927c3105203c70666b2aee
h5r <h5rcode@gmail.com>
2014-10-15 00:14:40 +0200

    BookCategoriesEdit does not extend ListEdit anymore.

--- src/com/blackbooks/activities/BookCategoriesEdit.java
+++ src/com/blackbooks/activities/BookCategoriesEdit.java
@@ -1,8 +1,19 @@
 package com.blackbooks.activities;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.blackbooks.R;
 import com.blackbooks.adapters.AutoCompleteAdapter;
@@ -12,64 +23,162 @@ import com.blackbooks.database.SQLiteHelper;
 import com.blackbooks.model.persistent.Category;
 import com.blackbooks.services.CategoryServices;
 
-public class BookCategoriesEdit extends ListEdit<Category> {
+public class BookCategoriesEdit extends Activity {
 
 	public static final String EXTRA_CATEGORY_LIST = "EXTRA_CATEGORY_LIST";
 
+	public static final String EXTRA_BOOK_TITLE = "EXTRA_BOOK_TITLE";
+
+	private static final String BOOK_TITLE = "BOOK_TITLE";
+
+	private static final String CATEGORY_LIST = "CATEGORY_LIST";
+
+	private String mBookTitle;
+	private ArrayList<Category> mCategoryList;
+	private LinkedHashMap<String, Category> mCategoryMap;
+	private AutoCompleteAdapter<Category> mAutoCompleteAdapter;
+	private EditableArrayAdapter<Category> mEditableArrayAdapter;
+
+	private TextView mTextInfo;
+	private AutoCompleteTextView mTextCategory;
+	private ListView mListObjects;
+
 	@Override
-	protected int getActionIdSave() {
-		return R.id.bookCategoriesEdit_actionSave;
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.book_categories_edit, menu);
+		return true;
 	}
 
 	@Override
-	protected int getListViewId() {
-		return R.id.bookCategoriesEdit_categoryList;
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		boolean result = false;
+		int itemId = item.getItemId();
+
+		if (itemId == R.id.bookCategoriesEdit_actionSave) {
+			addCategoryList();
+			result = true;
+		}
+		return result;
+	}
+
+	/**
+	 * Add the category to the list.
+	 * 
+	 * @param view
+	 *            View.
+	 */
+	public void addCategory(View view) {
+		String categoryName = mTextCategory.getText().toString().trim();
+
+		if (categoryName.length() == 0) {
+			mTextCategory.setError(getString(R.string.message_category_missing));
+		} else {
+			if (mCategoryMap.containsKey(categoryName)) {
+				String message = getString(R.string.message_category_already_present);
+				message = String.format(message, categoryName);
+				mTextCategory.setError(message);
+			} else {
+				Category object = getCategory(categoryName);
+
+				mCategoryMap.put(categoryName, object);
+				mEditableArrayAdapter.add(object);
+
+				String message = getString(R.string.message_category_added);
+				message = String.format(message, categoryName);
+				Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+				mTextCategory.setText(null);
+				mTextCategory.setError(null);
+			}
+		}
+	}
+
+	/**
+	 * Remove a category from the list.
+	 * 
+	 * @param view
+	 *            View.
+	 */
+	public void removeCategory(View view) {
+		Category object = (Category) view.getTag();
+		mCategoryMap.remove(object.name);
+		mEditableArrayAdapter.remove(object);
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_book_categories_edit);
+
+		if (savedInstanceState != null) {
+			mBookTitle = savedInstanceState.getString(BOOK_TITLE);
+			mCategoryList = (ArrayList<Category>) savedInstanceState.getSerializable(CATEGORY_LIST);
+		} else {
+			Intent intent = getIntent();
+			mBookTitle = intent.getStringExtra(EXTRA_BOOK_TITLE);
+			mCategoryList = (ArrayList<Category>) intent.getSerializableExtra(EXTRA_CATEGORY_LIST);
+		}
+		if (mCategoryList == null) {
+			mCategoryList = new ArrayList<Category>();
+		}
+
+		mCategoryMap = new LinkedHashMap<String, Category>();
+		for (Category object : mCategoryList) {
+			mCategoryMap.put(object.name, object);
+		}
+
+		mAutoCompleteAdapter = new AutoCompleteAdapter<Category>(this, android.R.layout.simple_list_item_1, new AutoCompleteSearcher<Category>() {
+
+			public ArrayList<Category> search(CharSequence constraint) {
+				SQLiteHelper mDbHelper = new SQLiteHelper(BookCategoriesEdit.this);
+				SQLiteDatabase db = mDbHelper.getReadableDatabase();
+				ArrayList<Category> categoryList = CategoryServices.getCategoryListByText(db, constraint.toString());
+				db.close();
+				return categoryList;
+			}
+
+			public String getDisplayLabel(Category item) {
+				return item.name;
+			}
+		});
+		mEditableArrayAdapter = new EditableArrayAdapter<Category>(this, R.id.bookCategoriesEdit_categoryList, R.layout.list_categories_item_category,
+				R.id.item_category_name, R.id.item_category_button_remove, mCategoryList) {
+
+			protected String getDisplayLabel(Category object) {
+				return object.name;
+			}
+		};
+
+		mTextInfo = (TextView) findViewById(R.id.bookCategoriesEdit_textInfo);
+		mTextCategory = (AutoCompleteTextView) findViewById(R.id.bookCategoriesEdit_textCategory);
+		mListObjects = (ListView) findViewById(R.id.bookCategoriesEdit_categoryList);
+
+		if (mBookTitle == null || mBookTitle.trim().equals("")) {
+			mTextInfo.setText(getString(R.string.text_info_edit_categories_untitled_book));
+		} else {
+			String message = getString(R.string.text_info_edit_categories);
+			message = String.format(message, mBookTitle.trim());
+			mTextInfo.setText(message);
+		}
+		mTextCategory.setAdapter(mAutoCompleteAdapter);
+		mListObjects.setAdapter(mEditableArrayAdapter);
+
+		TextView emptyText = (TextView) findViewById(android.R.id.empty);
+		mListObjects.setEmptyView(emptyText);
 	}
 
 	@Override
-	protected int getAutoCompleteTextViewId() {
-		return R.id.bookCategoriesEdit_textCategory;
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putString(BOOK_TITLE, mBookTitle);
+		outState.putSerializable(CATEGORY_LIST, mCategoryList);
 	}
 
-	@Override
-	protected int getTextViewIdInfo() {
-		return R.id.bookCategoriesEdit_textInfo;
-	}
-
-	@Override
-	protected int getLayoutIdActivity() {
-		return R.layout.activity_book_categories_edit;
-	}
-
-	@Override
-	protected int getStringIdObjectAdded() {
-		return R.string.message_category_added;
-	}
-
-	@Override
-	protected int getStringIdObjectAlreadyPresent() {
-		return R.string.message_category_already_present;
-	}
-
-	@Override
-	protected int getStringIdObjectMissing() {
-		return R.string.message_category_missing;
-	}
-	
-	@Override
-	protected int getStringIdEditBook() {
-		return R.string.text_info_edit_categories;
-	}
-	
-	@Override
-	protected int getStringIdEditUntitledBook() {
-		return R.string.text_info_edit_categories_untitled_book;
-	}
-
-	@Override
-	protected Category getObject(String objectKey) {
+	private Category getCategory(String categoryName) {
 		Category category = new Category();
-		category.name = objectKey;
+		category.name = categoryName;
 
 		SQLiteHelper dbHelper = new SQLiteHelper(this);
 		SQLiteDatabase db = dbHelper.getReadableDatabase();
@@ -81,55 +190,15 @@ public class BookCategoriesEdit extends ListEdit<Category> {
 		return category;
 	}
 
-	@Override
-	protected String getObjectKey(Category object) {
-		return object.name;
-	}
-
-	@Override
-	protected AutoCompleteAdapter<Category> getAutoCompleteAdapter() {
-		return new AutoCompleteAdapter<Category>(this, android.R.layout.simple_list_item_1, new AutoCompleteSearcher<Category>() {
-
-			@Override
-			public ArrayList<Category> search(CharSequence constraint) {
-				SQLiteHelper mDbHelper = new SQLiteHelper(BookCategoriesEdit.this);
-				SQLiteDatabase db = mDbHelper.getReadableDatabase();
-				ArrayList<Category> categoryList = CategoryServices.getCategoryListByText(db, constraint.toString());
-				db.close();
-				return categoryList;
-			}
-
-			@Override
-			public String getDisplayLabel(Category item) {
-				return item.name;
-			}
-		});
-	}
-
-	@Override
-	protected EditableArrayAdapter<Category> getEditableArrayAdapter() {
-		return new EditableArrayAdapter<Category>(this, R.id.bookCategoriesEdit_categoryList, R.layout.list_categories_item_category, R.id.item_category_name,
-				R.id.item_category_button_remove, mObjectList) {
-
-			@Override
-			protected String getDisplayLabel(Category object) {
-				return object.name;
-			}
-		};
-	}
-
-	@Override
-	protected String getExtraObjectListName() {
-		return EXTRA_CATEGORY_LIST;
-	}
-
-	@Override
-	protected int getMenuId() {
-		return R.menu.book_categories_edit;
-	}
-
-	@Override
-	protected String getSavedInstanceObjectListName() {
-		return "CATEGORY_LIST";
+	/**
+	 * Finish the activity and add the list of authors to the result of the
+	 * activity.
+	 */
+	private void addCategoryList() {
+		Intent intent = new Intent();
+		ArrayList<Category> objectList = new ArrayList<Category>(mCategoryMap.values());
+		intent.putExtra(EXTRA_CATEGORY_LIST, objectList);
+		setResult(RESULT_OK, intent);
+		finish();
 	}
 }
