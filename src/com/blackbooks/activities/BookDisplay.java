2c282a53789b6ab3b36c55344682e61215dfd6d1
h5r <h5rcode@gmail.com>
2014-11-05 22:40:52 +0100

    Added BookDisplayFragment, activity BookDisplay is now just a fragment container.

--- src/com/blackbooks/activities/BookDisplay.java
+++ src/com/blackbooks/activities/BookDisplay.java
@@ -1,139 +1,57 @@
 package com.blackbooks.activities;
 
-import java.util.Locale;
-
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
+import android.app.FragmentManager;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.blackbooks.R;
-import com.blackbooks.database.SQLiteHelper;
+import com.blackbooks.fragments.BookDisplayFragment;
+import com.blackbooks.fragments.BookDisplayFragment.BookDisplayListener;
 import com.blackbooks.model.nonpersistent.BookInfo;
-import com.blackbooks.services.BookServices;
-import com.blackbooks.utils.StringUtils;
-import com.blackbooks.utils.VariableUtils;
 
 /**
  * Activity to display the info of a book saved in the database.
  * 
  */
-public final class BookDisplay extends Activity {
+public final class BookDisplay extends Activity implements BookDisplayListener {
 
-	public final static String EXTRA_BOOK_ID = "BOOK_ID";
+	/**
+	 * Key of the book id when passed as an extra of the activity.
+	 */
+	public final static String EXTRA_BOOK_ID = "EXTRA_BOOK_ID";
 
-	private final static int REQUEST_CODE_EDIT_BOOK = 1;
-
-	private SQLiteHelper mDbHelper;
-
-	private BookInfo mBookInfo;
-
-	private ImageView mImageCover;
-	private TextView mTextTitle;
-	private TextView mTextSubtitle;
-	private TextView mTextAuthor;
-	private TextView mTextIsbn;
-	private TextView mTextPageCount;
-	private TextView mTextPublisher;
-	private TextView mTextPublishedDate;
-	private TextView mTextCategory;
-	private TextView mTextLanguage;
-	private TextView mTextDescription;
-
-	private LinearLayout mGroupInfo;
-	private LinearLayout mGroupInfoUserFriendlyPageCount;
-	private LinearLayout mGroupInfoUserFriendlyCategories;
-	private LinearLayout mGroupInfoUserFriendlyLanguage;
-	private LinearLayout mGroupInfoTechnicalIsbn;
-	private LinearLayout mGroupInfoTechnicalPublisher;
-	private LinearLayout mGroupInfoTechnicalPublishedDate;
-	private LinearLayout mGroupDescription;
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-
-		if (resultCode == RESULT_OK) {
-			if (requestCode == REQUEST_CODE_EDIT_BOOK) {
-				this.recreate();
-			}
-		}
-	}
+	private final static String BOOK_DISPLAY_FRAGMENT_TAG = "BOOK_DISPLAY_FRAGMENT_TAG";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		setContentView(R.layout.activity_book_display);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 
-		Intent intent = getIntent();
+		FragmentManager fm = getFragmentManager();
+		BookDisplayFragment fragment = (BookDisplayFragment) fm.findFragmentByTag(BOOK_DISPLAY_FRAGMENT_TAG);
+		if (fragment == null) {
+			Intent intent = this.getIntent();
+			if (intent != null && intent.hasExtra(EXTRA_BOOK_ID)) {
+				long bookId = intent.getLongExtra(EXTRA_BOOK_ID, Long.MIN_VALUE);
+				fragment = BookDisplayFragment.newInstance(bookId);
 
-		if (intent != null && intent.hasExtra(EXTRA_BOOK_ID)) {
-			long bookId = intent.getLongExtra(EXTRA_BOOK_ID, Long.MIN_VALUE);
-
-			mDbHelper = new SQLiteHelper(this);
-			loadBook(bookId);
-
-			setContentView(R.layout.activity_book_display);
-
-			setTitle(mBookInfo.title);
-			findViews();
-			renderBookInfo();
-
-		} else {
-			finish();
+				fm.beginTransaction() //
+						.add(R.id.bookDisplay_frameLayout, fragment, BOOK_DISPLAY_FRAGMENT_TAG) //
+						.commit();
+			} else {
+				this.finish();
+			}
 		}
-	}
-
-	/**
-	 * Delete the current book.
-	 */
-	public void delete() {
-		String title = mBookInfo.title;
-		String message = String.format(getString(R.string.message_book_deleted), title);
-
-		SQLiteDatabase db = mDbHelper.getWritableDatabase();
-		BookServices.deleteBook(db, mBookInfo.id);
-		db.close();
-		VariableUtils.getInstance().setReloadBookList(true);
-		Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-		finish();
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.book_display, menu);
-		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		boolean result;
 		switch (item.getItemId()) {
-		case R.id.bookDisplay_actionEdit:
-			Intent i = new Intent(this, BookEdit.class);
-			i.putExtra(BookEdit.EXTRA_BOOK_ID, mBookInfo.id);
-			startActivityForResult(i, REQUEST_CODE_EDIT_BOOK);
-			result = true;
-			break;
-
-		case R.id.bookDisplay_actionDelete:
-			showDeleteConfirmDialog();
-			result = true;
-			break;
 		case android.R.id.home:
 			NavUtils.navigateUpFromSameTask(this);
 			result = true;
@@ -145,145 +63,13 @@ public final class BookDisplay extends Activity {
 		return result;
 	}
 
-	/**
-	 * Find the views of the activity that will contain the book information.
-	 */
-	private void findViews() {
-		mImageCover = (ImageView) findViewById(R.id.bookDisplay_imageCover);
-		mTextTitle = (TextView) findViewById(R.id.bookDisplay_textTitle);
-		mTextSubtitle = (TextView) findViewById(R.id.bookDisplay_textSubtitle);
-		mTextAuthor = (TextView) findViewById(R.id.bookDisplay_textAuthor);
-		mTextIsbn = (TextView) findViewById(R.id.bookDisplay_textIsbn);
-		mTextPageCount = (TextView) findViewById(R.id.bookDisplay_textPageCount);
-		mTextPublisher = (TextView) findViewById(R.id.bookDisplay_textPublisher);
-		mTextPublishedDate = (TextView) findViewById(R.id.bookDisplay_textPublishedDate);
-		mTextCategory = (TextView) findViewById(R.id.bookDisplay_textCategory);
-		mTextLanguage = (TextView) findViewById(R.id.bookDisplay_textLanguage);
-		mTextLanguage = (TextView) findViewById(R.id.bookDisplay_textLanguage);
-		mTextDescription = (TextView) findViewById(R.id.bookDisplay_textDescription);
-
-		mGroupInfo = (LinearLayout) findViewById(R.id.bookDisplay_groupInfo);
-		mGroupInfoUserFriendlyPageCount = (LinearLayout) findViewById(R.id.bookDisplay_groupInfoUserFriendly_pageCount);
-		mGroupInfoUserFriendlyCategories = (LinearLayout) findViewById(R.id.bookDisplay_groupInfoUserFriendly_categories);
-		mGroupInfoUserFriendlyLanguage = (LinearLayout) findViewById(R.id.bookDisplay_groupInfoUserFriendly_language);
-		mGroupInfoTechnicalIsbn = (LinearLayout) findViewById(R.id.bookDisplay_groupInfoTechnical_isbn);
-		mGroupInfoTechnicalPublisher = (LinearLayout) findViewById(R.id.bookDisplay_groupInfoTechnical_publisher);
-		mGroupInfoTechnicalPublishedDate = (LinearLayout) findViewById(R.id.bookDisplay_groupInfoTechnical_publishedDate);
-		mGroupDescription = (LinearLayout) findViewById(R.id.bookDisplay_groupDescription);
+	@Override
+	public void onBookLoaded(BookInfo bookInfo) {
+		this.setTitle(bookInfo.title);
 	}
 
-	/**
-	 * Update the views of the activity using the book information.
-	 */
-	private void renderBookInfo() {
-		boolean hasPageCount = mBookInfo.pageCount != null;
-		boolean hasCategories = mBookInfo.categories.size() > 0;
-		boolean hasLanguage = mBookInfo.languageCode != null;
-		boolean hasIdentifiers = mBookInfo.identifiers.size() > 0;
-		boolean hasPublisher = mBookInfo.publisher.name != null;
-		boolean hasPublishedDate = mBookInfo.publishedDate != null;
-
-		boolean showInfoUserFriendly = hasPageCount || hasCategories || hasLanguage;
-		boolean showInfoTechnical = hasIdentifiers || hasPublisher || hasPublishedDate;
-		boolean showInfo = showInfoUserFriendly || showInfoTechnical;
-
-		mTextTitle.setText(mBookInfo.title);
-		if (mBookInfo.thumbnail != null && mBookInfo.thumbnail.length > 0) {
-			Bitmap bitmap = BitmapFactory.decodeByteArray(mBookInfo.thumbnail, 0, mBookInfo.thumbnail.length);
-			mImageCover.setImageBitmap(bitmap);
-		} else {
-			mImageCover.setVisibility(View.GONE);
-		}
-		if (mBookInfo.subtitle != null) {
-			mTextSubtitle.setText(mBookInfo.subtitle);
-		} else {
-			mTextSubtitle.setVisibility(View.GONE);
-		}
-		if (mBookInfo.authors.size() > 0) {
-			String authorsFormat = getString(R.string.label_authors_format);
-			String authors = String.format(authorsFormat, StringUtils.joinAuthorNameList(mBookInfo.authors, ", "));
-			mTextAuthor.setText(authors);
-		} else {
-			mTextAuthor.setText(getString(R.string.label_unspecified_author));
-		}
-
-		if (showInfo) {
-			if (hasPageCount) {
-				mTextPageCount.setText(mBookInfo.pageCount.toString());
-			} else {
-				mGroupInfoUserFriendlyPageCount.setVisibility(View.GONE);
-			}
-			if (hasCategories) {
-				mTextCategory.setText(StringUtils.joinCategoryNameList(mBookInfo.categories, ", "));
-			} else {
-				mGroupInfoUserFriendlyCategories.setVisibility(View.GONE);
-			}
-			if (hasLanguage) {
-				Locale locale = new Locale(mBookInfo.languageCode);
-				String language = locale.getDisplayLanguage();
-				mTextLanguage.setText(StringUtils.capitalize(language));
-			} else {
-				mGroupInfoUserFriendlyLanguage.setVisibility(View.GONE);
-			}
-			if (hasIdentifiers) {
-				mTextIsbn.setText(mBookInfo.identifiers.get(0).identifier);
-			} else {
-				mGroupInfoTechnicalIsbn.setVisibility(View.GONE);
-			}
-			if (hasPublisher) {
-				mTextPublisher.setText(mBookInfo.publisher.name);
-			} else {
-				mGroupInfoTechnicalPublisher.setVisibility(View.GONE);
-			}
-			if (hasPublishedDate) {
-				mTextPublishedDate.setText(mBookInfo.publishedDate);
-			} else {
-				mGroupInfoTechnicalPublishedDate.setVisibility(View.GONE);
-			}
-		} else {
-			mGroupInfo.setVisibility(View.GONE);
-		}
-		if (mBookInfo.description != null) {
-			mTextDescription.setText(mBookInfo.description);
-
-		} else {
-			mGroupDescription.setVisibility(View.GONE);
-		}
-	}
-
-	/**
-	 * Load the book from the database.
-	 * 
-	 * @param bookId
-	 */
-	private void loadBook(long bookId) {
-		SQLiteDatabase db = mDbHelper.getReadableDatabase();
-		mBookInfo = BookServices.getBookInfo(db, bookId);
-		db.close();
-	}
-
-	/**
-	 * Show the delete confirm dialog.
-	 */
-	private void showDeleteConfirmDialog() {
-		String message = getString(R.string.message_confirm_delete_book);
-		message = String.format(message, mBookInfo.title);
-
-		String cancelText = getString(R.string.message_confirm_delete_book_cancel);
-		String confirmText = getString(R.string.message_confirm_delete_book_confirm);
-
-		new AlertDialog.Builder(this).setMessage(message).setPositiveButton(confirmText, new OnClickListener() {
-
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				BookDisplay.this.delete();
-			}
-		}).setNegativeButton(cancelText, new OnClickListener() {
-
-			@Override
-			public void onClick(DialogInterface dialog, int which) {
-				// Do nothing.
-			}
-		}).show();
+	@Override
+	public void onBookDeleted() {
+		this.finish();
 	}
 }
---
+++ src/com/blackbooks/fragments/BookDisplayFragment.java
@@ -0,0 +1,352 @@
+package com.blackbooks.fragments;
+
+import java.util.Locale;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.blackbooks.R;
+import com.blackbooks.activities.BookEdit;
+import com.blackbooks.database.SQLiteHelper;
+import com.blackbooks.model.nonpersistent.BookInfo;
+import com.blackbooks.services.BookServices;
+import com.blackbooks.utils.StringUtils;
+import com.blackbooks.utils.VariableUtils;
+
+/**
+ * Fragment to display the information of a book.
+ */
+public class BookDisplayFragment extends Fragment {
+
+	private final static String ARG_BOOK_ID = "ARG_BOOK_ID";
+
+	private final static int REQUEST_CODE_EDIT_BOOK = 1;
+
+	private SQLiteHelper mDbHelper;
+
+	private BookInfo mBookInfo;
+
+	private ImageView mImageCover;
+	private TextView mTextTitle;
+	private TextView mTextSubtitle;
+	private TextView mTextAuthor;
+	private TextView mTextIsbn;
+	private TextView mTextPageCount;
+	private TextView mTextPublisher;
+	private TextView mTextPublishedDate;
+	private TextView mTextCategory;
+	private TextView mTextLanguage;
+	private TextView mTextDescription;
+
+	private LinearLayout mGroupInfo;
+	private LinearLayout mGroupInfoUserFriendlyPageCount;
+	private LinearLayout mGroupInfoUserFriendlyCategories;
+	private LinearLayout mGroupInfoUserFriendlyLanguage;
+	private LinearLayout mGroupInfoTechnicalIsbn;
+	private LinearLayout mGroupInfoTechnicalPublisher;
+	private LinearLayout mGroupInfoTechnicalPublishedDate;
+	private LinearLayout mGroupDescription;
+
+	private BookDisplayListener mBookDisplayListener;
+
+	/**
+	 * Create a new instance of BookDisplayFragment, initialized to display a
+	 * book.
+	 * 
+	 * @param bookId
+	 *            Id of the book.
+	 * @return BookDisplayFragment.
+	 */
+	public static BookDisplayFragment newInstance(long bookId) {
+		BookDisplayFragment instance = new BookDisplayFragment();
+		Bundle args = new Bundle();
+		args.putLong(ARG_BOOK_ID, bookId);
+		instance.setArguments(args);
+		return instance;
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		if (activity instanceof BookDisplayListener) {
+			mBookDisplayListener = (BookDisplayListener) activity;
+		}
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRetainInstance(true);
+		setHasOptionsMenu(true);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_book_display, container, false);
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+		Bundle args = getArguments();
+		if (args != null) {
+			long bookId = args.getLong(ARG_BOOK_ID);
+			mDbHelper = new SQLiteHelper(this.getActivity());
+			findViews();
+			displayBook(bookId);
+		}
+	}
+
+	/**
+	 * Load the book info from the database and render it.
+	 * 
+	 * @param bookId
+	 *            Id of the book.
+	 */
+	private void displayBook(long bookId) {
+		loadBook(bookId);
+		renderBookInfo();
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (resultCode == Activity.RESULT_OK) {
+			if (requestCode == REQUEST_CODE_EDIT_BOOK) {
+				displayBook(mBookInfo.id);
+			}
+		}
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		super.onCreateOptionsMenu(menu, inflater);
+		inflater.inflate(R.menu.book_display, menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		boolean result;
+		switch (item.getItemId()) {
+		case R.id.bookDisplay_actionEdit:
+			Intent i = new Intent(this.getActivity(), BookEdit.class);
+			i.putExtra(BookEdit.EXTRA_BOOK_ID, mBookInfo.id);
+			startActivityForResult(i, REQUEST_CODE_EDIT_BOOK);
+			result = true;
+			break;
+
+		case R.id.bookDisplay_actionDelete:
+			showDeleteConfirmDialog();
+			result = true;
+			break;
+		default:
+			result = super.onOptionsItemSelected(item);
+			break;
+		}
+		return result;
+	}
+
+	/**
+	 * Find the views of the activity that will contain the book information.
+	 */
+	private void findViews() {
+		View rootView = getView();
+
+		mImageCover = (ImageView) rootView.findViewById(R.id.bookDisplay_imageCover);
+		mTextTitle = (TextView) rootView.findViewById(R.id.bookDisplay_textTitle);
+		mTextSubtitle = (TextView) rootView.findViewById(R.id.bookDisplay_textSubtitle);
+		mTextAuthor = (TextView) rootView.findViewById(R.id.bookDisplay_textAuthor);
+		mTextIsbn = (TextView) rootView.findViewById(R.id.bookDisplay_textIsbn);
+		mTextPageCount = (TextView) rootView.findViewById(R.id.bookDisplay_textPageCount);
+		mTextPublisher = (TextView) rootView.findViewById(R.id.bookDisplay_textPublisher);
+		mTextPublishedDate = (TextView) rootView.findViewById(R.id.bookDisplay_textPublishedDate);
+		mTextCategory = (TextView) rootView.findViewById(R.id.bookDisplay_textCategory);
+		mTextLanguage = (TextView) rootView.findViewById(R.id.bookDisplay_textLanguage);
+		mTextLanguage = (TextView) rootView.findViewById(R.id.bookDisplay_textLanguage);
+		mTextDescription = (TextView) rootView.findViewById(R.id.bookDisplay_textDescription);
+
+		mGroupInfo = (LinearLayout) rootView.findViewById(R.id.bookDisplay_groupInfo);
+		mGroupInfoUserFriendlyPageCount = (LinearLayout) rootView.findViewById(R.id.bookDisplay_groupInfoUserFriendly_pageCount);
+		mGroupInfoUserFriendlyCategories = (LinearLayout) rootView.findViewById(R.id.bookDisplay_groupInfoUserFriendly_categories);
+		mGroupInfoUserFriendlyLanguage = (LinearLayout) rootView.findViewById(R.id.bookDisplay_groupInfoUserFriendly_language);
+		mGroupInfoTechnicalIsbn = (LinearLayout) rootView.findViewById(R.id.bookDisplay_groupInfoTechnical_isbn);
+		mGroupInfoTechnicalPublisher = (LinearLayout) rootView.findViewById(R.id.bookDisplay_groupInfoTechnical_publisher);
+		mGroupInfoTechnicalPublishedDate = (LinearLayout) rootView.findViewById(R.id.bookDisplay_groupInfoTechnical_publishedDate);
+		mGroupDescription = (LinearLayout) rootView.findViewById(R.id.bookDisplay_groupDescription);
+	}
+
+	/**
+	 * Load the book from the database.
+	 * 
+	 * @param bookId
+	 *            Id of a book.
+	 */
+	private void loadBook(long bookId) {
+		SQLiteDatabase db = mDbHelper.getReadableDatabase();
+		mBookInfo = BookServices.getBookInfo(db, bookId);
+		db.close();
+		
+		if (mBookDisplayListener != null) {
+			mBookDisplayListener.onBookLoaded(mBookInfo);
+		}
+	}
+
+	/**
+	 * Update the views of the activity using the book information.
+	 */
+	private void renderBookInfo() {
+		boolean hasPageCount = mBookInfo.pageCount != null;
+		boolean hasCategories = mBookInfo.categories.size() > 0;
+		boolean hasLanguage = mBookInfo.languageCode != null;
+		boolean hasIdentifiers = mBookInfo.identifiers.size() > 0;
+		boolean hasPublisher = mBookInfo.publisher.name != null;
+		boolean hasPublishedDate = mBookInfo.publishedDate != null;
+
+		boolean showInfoUserFriendly = hasPageCount || hasCategories || hasLanguage;
+		boolean showInfoTechnical = hasIdentifiers || hasPublisher || hasPublishedDate;
+		boolean showInfo = showInfoUserFriendly || showInfoTechnical;
+
+		mTextTitle.setText(mBookInfo.title);
+		if (mBookInfo.thumbnail != null && mBookInfo.thumbnail.length > 0) {
+			Bitmap bitmap = BitmapFactory.decodeByteArray(mBookInfo.thumbnail, 0, mBookInfo.thumbnail.length);
+			mImageCover.setImageBitmap(bitmap);
+		} else {
+			mImageCover.setVisibility(View.GONE);
+		}
+		if (mBookInfo.subtitle != null) {
+			mTextSubtitle.setText(mBookInfo.subtitle);
+		} else {
+			mTextSubtitle.setVisibility(View.GONE);
+		}
+		if (mBookInfo.authors.size() > 0) {
+			String authorsFormat = getString(R.string.label_authors_format);
+			String authors = String.format(authorsFormat, StringUtils.joinAuthorNameList(mBookInfo.authors, ", "));
+			mTextAuthor.setText(authors);
+		} else {
+			mTextAuthor.setText(getString(R.string.label_unspecified_author));
+		}
+
+		if (showInfo) {
+			if (hasPageCount) {
+				mTextPageCount.setText(mBookInfo.pageCount.toString());
+			} else {
+				mGroupInfoUserFriendlyPageCount.setVisibility(View.GONE);
+			}
+			if (hasCategories) {
+				mTextCategory.setText(StringUtils.joinCategoryNameList(mBookInfo.categories, ", "));
+			} else {
+				mGroupInfoUserFriendlyCategories.setVisibility(View.GONE);
+			}
+			if (hasLanguage) {
+				Locale locale = new Locale(mBookInfo.languageCode);
+				String language = locale.getDisplayLanguage();
+				mTextLanguage.setText(StringUtils.capitalize(language));
+			} else {
+				mGroupInfoUserFriendlyLanguage.setVisibility(View.GONE);
+			}
+			if (hasIdentifiers) {
+				mTextIsbn.setText(mBookInfo.identifiers.get(0).identifier);
+			} else {
+				mGroupInfoTechnicalIsbn.setVisibility(View.GONE);
+			}
+			if (hasPublisher) {
+				mTextPublisher.setText(mBookInfo.publisher.name);
+			} else {
+				mGroupInfoTechnicalPublisher.setVisibility(View.GONE);
+			}
+			if (hasPublishedDate) {
+				mTextPublishedDate.setText(mBookInfo.publishedDate);
+			} else {
+				mGroupInfoTechnicalPublishedDate.setVisibility(View.GONE);
+			}
+		} else {
+			mGroupInfo.setVisibility(View.GONE);
+		}
+		if (mBookInfo.description != null) {
+			mTextDescription.setText(mBookInfo.description);
+
+		} else {
+			mGroupDescription.setVisibility(View.GONE);
+		}
+	}
+
+	/**
+	 * Delete the current book.
+	 */
+	public void delete() {
+		String title = mBookInfo.title;
+		String message = String.format(getString(R.string.message_book_deleted), title);
+
+		SQLiteDatabase db = mDbHelper.getWritableDatabase();
+		BookServices.deleteBook(db, mBookInfo.id);
+		db.close();
+		VariableUtils.getInstance().setReloadBookList(true);
+		Toast.makeText(this.getActivity(), message, Toast.LENGTH_SHORT).show();
+		
+		if (mBookDisplayListener != null) {
+			mBookDisplayListener.onBookDeleted();
+		}
+	}
+
+	/**
+	 * Show the delete confirm dialog.
+	 */
+	private void showDeleteConfirmDialog() {
+		String message = getString(R.string.message_confirm_delete_book);
+		message = String.format(message, mBookInfo.title);
+
+		String cancelText = getString(R.string.message_confirm_delete_book_cancel);
+		String confirmText = getString(R.string.message_confirm_delete_book_confirm);
+
+		new AlertDialog.Builder(this.getActivity()).setMessage(message).setPositiveButton(confirmText, new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				BookDisplayFragment.this.delete();
+			}
+		}).setNegativeButton(cancelText, new OnClickListener() {
+
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// Do nothing.
+			}
+		}).show();
+	}
+
+	/**
+	 * An activity hosting a {@link BookDisplayFragment} should implement this
+	 * interface to be notified when the book is loaded, deleted.
+	 */
+	public interface BookDisplayListener {
+		
+		/**
+		 * Called when the book is loaded.
+		 * @param bookInfo BookInfo.
+		 */
+		void onBookLoaded(BookInfo bookInfo);
+
+		/**
+		 * Called when the book is deleted.
+		 */
+		void onBookDeleted();
+	}
+}
