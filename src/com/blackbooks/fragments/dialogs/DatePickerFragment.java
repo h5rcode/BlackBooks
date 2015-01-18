31d8f195dab222c9f442dec48c8a3faf2c60cbfd
h5r <h5rcode@gmail.com>
2015-01-18 23:20:04 +0100

    First commit from Android Studio.

--- src/com/blackbooks/fragments/dialogs/DatePickerFragment.java
+++ src/com/blackbooks/fragments/dialogs/DatePickerFragment.java
@@ -1,42 +1,51 @@
 package com.blackbooks.fragments.dialogs;
 
-import java.util.Calendar;
-import java.util.Date;
-
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.widget.DatePicker;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
-	private DatePickerListener mDatePickerListener;
+    private DatePickerListener mDatePickerListener;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		mDatePickerListener = (DatePickerListener) getTargetFragment();
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mDatePickerListener = (DatePickerListener) getTargetFragment();
+    }
 
-	@Override
-	public Dialog onCreateDialog(Bundle savedInstanceState) {
-		Calendar c = Calendar.getInstance();
-		int year = c.get(Calendar.YEAR);
-		int month = c.get(Calendar.MONTH);
-		int day = c.get(Calendar.DAY_OF_MONTH);
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
 
-		return new DatePickerDialog(getActivity(), this, year, month, day);
-	}
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
 
-	@Override
-	public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-		Calendar c = Calendar.getInstance();
-		c.set(year, monthOfYear, dayOfMonth);
-		mDatePickerListener.onDateSet(c.getTime());
-	}
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(year, monthOfYear, dayOfMonth);
+        mDatePickerListener.onDateSet(c.getTime());
+    }
 
-	public interface DatePickerListener {
-		void onDateSet(Date date);
-	}
+    /**
+     * Activities hosting a DatePickerFragment should implement this interface to be notified when a date is set.
+     */
+    public interface DatePickerListener {
+
+        /**
+         * Called when the date is set.
+         *
+         * @param date Date.
+         */
+        void onDateSet(Date date);
+    }
 }
