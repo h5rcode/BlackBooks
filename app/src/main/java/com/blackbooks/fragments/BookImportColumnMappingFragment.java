f6cf0a77b3f45fb9d888bf5b528363e7e631edbe
h5r <h5rcode@gmail.com>
2015-08-10 22:31:02 +0200

    Removed BookImportFileParsingActivity, the parsing now takes place in BookImportColumnMappingFragment.

--- app/src/main/java/com/blackbooks/fragments/BookImportColumnMappingFragment.java
+++ app/src/main/java/com/blackbooks/fragments/BookImportColumnMappingFragment.java
@@ -1,8 +1,12 @@
 package com.blackbooks.fragments;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -15,33 +19,41 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.blackbooks.R;
-import com.blackbooks.activities.BookImportFileParsingActivity;
+import com.blackbooks.activities.SummaryActivity;
 import com.blackbooks.adapters.CsvColumnListAdapter;
+import com.blackbooks.database.SQLiteHelper;
 import com.blackbooks.fragments.dialogs.ColumnSeparator;
+import com.blackbooks.fragments.dialogs.ProgressDialogFragment;
 import com.blackbooks.fragments.dialogs.TextQualifier;
+import com.blackbooks.model.nonpersistent.BookInfo;
 import com.blackbooks.model.nonpersistent.CsvColumn;
+import com.blackbooks.services.BookServices;
 import com.blackbooks.utils.CsvUtils;
+import com.blackbooks.utils.LogUtils;
 
 import java.io.File;
-import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * The fragment that allows the user to map each column of a CSV file to a property of a book.
  */
-public final class BookImportColumnMappingFragment extends Fragment {
+public final class BookImportColumnMappingFragment extends Fragment implements ProgressDialogFragment.OnProgressDialogListener {
 
     private static final String ARG_FILE = "ARG_FILE";
     private static final String ARG_COLUMN_SEPARATOR = "ARG_COLUMN_SEPARATOR";
     private static final String ARG_TEXT_QUALIFIER = "ARG_TEXT_QUALIFIER";
     private static final String ARG_FIRST_ROW_CONTAINS_HEADER = "ARG_FIRST_ROW_CONTAINS_HEADER";
+    private static final String TAG_PROGRESS_DIALOG_FRAGMENT = "TAG_PROGRESS_DIALOG_FRAGMENT";
 
     private File mFile;
     private ColumnSeparator mColumnSeparator;
     private TextQualifier mTextQualifier;
     private boolean mFirstRowContainsHeader;
     private List<CsvColumn> mCsvColumns;
+
+    private CsvParsingTask mCsvParsingTask;
+    private BookSavingTask mBookSavingTask;
 
     /**
      * Constructor.
@@ -103,27 +115,30 @@ public final class BookImportColumnMappingFragment extends Fragment {
     }
 
     @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cancelAsyncTasks();
+    }
+
+    @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.book_import, menu);
+        inflater.inflate(R.menu.book_import_column_mapping, menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         boolean result;
         switch (item.getItemId()) {
-            case R.id.bookImport_actionNextStep:
+            case R.id.bookImportColumnMapping_actionImport:
                 result = true;
 
                 boolean ok = checkMappings();
                 if (ok) {
-                    Intent i = new Intent(getActivity(), BookImportFileParsingActivity.class);
-                    i.putExtra(BookImportFileParsingActivity.EXTRA_COLUMN_SEPARATOR, mColumnSeparator);
-                    i.putExtra(BookImportFileParsingActivity.EXTRA_CSV_COLUMNS, (Serializable) mCsvColumns);
-                    i.putExtra(BookImportFileParsingActivity.EXTRA_FILE, mFile);
-                    i.putExtra(BookImportFileParsingActivity.EXTRA_FIRST_ROW_CONTAINS_HEADER, mFirstRowContainsHeader);
-                    i.putExtra(BookImportFileParsingActivity.EXTRA_TEXT_QUALIFIER, mTextQualifier);
-                    startActivity(i);
+                    mCsvParsingTask = new CsvParsingTask(
+                            mFile, mColumnSeparator.getCharacter(), mTextQualifier.getCharacter(),
+                            mFirstRowContainsHeader, mCsvColumns);
+                    mCsvParsingTask.execute();
                 }
 
                 break;
@@ -134,6 +149,23 @@ public final class BookImportColumnMappingFragment extends Fragment {
         }
 
         return result;
+    }
+
+    @Override
+    public void onCancel() {
+        cancelAsyncTasks();
+    }
+
+    /**
+     * Cancel the asynchronous tasks.
+     */
+    private void cancelAsyncTasks() {
+        if (mCsvParsingTask != null) {
+            mCsvParsingTask.cancel(true);
+        }
+        if (mBookSavingTask != null) {
+            mBookSavingTask.cancel(true);
+        }
     }
 
     /**
@@ -177,5 +209,145 @@ public final class BookImportColumnMappingFragment extends Fragment {
         }
 
         return ok;
+    }
+
+    /**
+     * The asynchronous task responsible for saving a list of books to the database.
+     */
+    private final class BookSavingTask extends AsyncTask<Void, Integer, Void> {
+
+        private final List<BookInfo> mBookInfos;
+        private final ProgressDialogFragment mProgressDialogFragment;
+
+        /**
+         * Constructor.
+         *
+         * @param bookInfos The books to save.
+         */
+        public BookSavingTask(List<BookInfo> bookInfos) {
+            mBookInfos = bookInfos;
+            mProgressDialogFragment = new ProgressDialogFragment();
+            mProgressDialogFragment.setTargetFragment(BookImportColumnMappingFragment.this, 0);
+            mProgressDialogFragment.setTitle(R.string.title_dialog_save_parsed_books);
+            mProgressDialogFragment.setMessage(R.string.message_save_parsed_books);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mProgressDialogFragment.setMax(mBookInfos.size());
+            FragmentManager fm = getActivity().getSupportFragmentManager();
+            mProgressDialogFragment.show(fm, TAG_PROGRESS_DIALOG_FRAGMENT);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            if (!mBookInfos.isEmpty()) {
+                final SQLiteDatabase db = SQLiteHelper.getInstance().getWritableDatabase();
+
+                db.beginTransaction();
+                try {
+                    int i = 0;
+                    boolean isCancelled = false;
+                    for (final BookInfo bookInfo : mBookInfos) {
+
+                        if (isCancelled = isCancelled()) {
+                            break;
+                        }
+
+                        try {
+                            BookServices.saveBookInfo(db, bookInfo);
+                        } catch (Exception e) {
+                            final String msg = String.format("Error when saving the book %s.", bookInfo.title);
+                            Log.e(LogUtils.TAG, msg, e);
+                            throw new RuntimeException(e);
+                        }
+
+                        i++;
+                        publishProgress(i);
+                    }
+
+                    if (!isCancelled) {
+                        db.setTransactionSuccessful();
+                    }
+                } finally {
+                    db.endTransaction();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            int progress = values[0];
+            mProgressDialogFragment.setProgress(progress);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            mProgressDialogFragment.dismiss();
+
+            Intent i = new Intent(getActivity(), SummaryActivity.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(i);
+        }
+    }
+
+    /**
+     * The asynchronous task that will parse the CSV file and return the list of books read from it.
+     */
+    private final class CsvParsingTask extends AsyncTask<Void, Void, List<BookInfo>> {
+
+        private final File mFile;
+        private final char mColumnSeparator;
+        private final char mTextQualifier;
+        private final boolean mFirstRowContainsHeader;
+        private final List<CsvColumn> mCsvColumns;
+
+        /**
+         * Constructor.
+         *
+         * @param file                   The CSV file to parse.
+         * @param columnSeparator        The column separator character.
+         * @param textQualifier          The text qualifier character.
+         * @param firstRowContainsHeader A boolean indicating whether the first row of the CSV file
+         *                               contains headers or not.
+         * @param csvColumns             The CSV columns mapping settings.
+         */
+        public CsvParsingTask(
+                File file, char columnSeparator, char textQualifier, boolean firstRowContainsHeader,
+                List<CsvColumn> csvColumns) {
+            mFile = file;
+            mColumnSeparator = columnSeparator;
+            mTextQualifier = textQualifier;
+            mFirstRowContainsHeader = firstRowContainsHeader;
+            mCsvColumns = csvColumns;
+        }
+
+        @Override
+        protected List<BookInfo> doInBackground(Void... voids) {
+            List<BookInfo> bookInfos = null;
+            try {
+                bookInfos = CsvUtils.parseCsvFile(mFile, mColumnSeparator, mTextQualifier, mFirstRowContainsHeader, mCsvColumns);
+            } catch (InterruptedException e) {
+                // Do nothing. The method will terminate and return an empty list of books.
+            } finally {
+                if (bookInfos == null) {
+                    bookInfos = new ArrayList<BookInfo>();
+                }
+            }
+            return bookInfos;
+        }
+
+        @Override
+        protected void onPostExecute(List<BookInfo> result) {
+            super.onPostExecute(result);
+            mBookSavingTask = new BookSavingTask(result);
+            mBookSavingTask.execute();
+        }
     }
 }
--- app/src/main/java/com/blackbooks/fragments/dialogs/ProgressDialogFragment.java
+++ app/src/main/java/com/blackbooks/fragments/dialogs/ProgressDialogFragment.java
@@ -15,13 +15,17 @@ public final class ProgressDialogFragment extends DialogFragment {
     private ProgressDialog mProgressDialog;
 
     private Integer mTitleId;
+    private Integer mMessageId;
     private Integer mMax;
-    private DialogInterface.OnCancelListener mOnCancelListener;
+
+    private OnProgressDialogListener mOnProgressDialogListener;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+
+        mOnProgressDialogListener = (OnProgressDialogListener) getTargetFragment();
     }
 
     @NonNull
@@ -33,8 +37,8 @@ public final class ProgressDialogFragment extends DialogFragment {
         if (mTitleId != null) {
             mProgressDialog.setTitle(mTitleId);
         }
-        if (mOnCancelListener != null) {
-            mProgressDialog.setOnCancelListener(mOnCancelListener);
+        if (mMessageId != null) {
+            mProgressDialog.setMessage(getString(mMessageId));
         }
         if (mMax != null) {
             mProgressDialog.setMax(mMax);
@@ -42,6 +46,11 @@ public final class ProgressDialogFragment extends DialogFragment {
         return mProgressDialog;
     }
 
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+        mOnProgressDialogListener.onCancel();
+    }
 
     @Override
     public void onDestroyView() {
@@ -73,20 +82,32 @@ public final class ProgressDialogFragment extends DialogFragment {
     }
 
     /**
-     * Set the cancel listener.
-     *
-     * @param onCancelListener Cancel listener.
-     */
-    public void setOnCancelListener(DialogInterface.OnCancelListener onCancelListener) {
-        mOnCancelListener = onCancelListener;
-    }
-
-    /**
      * Set the max value of the progress dialog.
      *
      * @param max Max value.
      */
     public void setMax(int max) {
         mMax = max;
+    }
+
+    /**
+     * Set the id of the string resource to use as the message of the dialog.
+     *
+     * @param messageId Id of the string to be used as the message.
+     */
+    public void setMessage(int messageId) {
+        mMessageId = messageId;
+    }
+
+    /**
+     * Fragments hosting a ProgressDialogFragment must implement this interface to be notified
+     * when the dialog is cancelled.
+     */
+    public interface OnProgressDialogListener {
+
+        /**
+         * Called when the dialog is cancelled.
+         */
+        void onCancel();
     }
 }
