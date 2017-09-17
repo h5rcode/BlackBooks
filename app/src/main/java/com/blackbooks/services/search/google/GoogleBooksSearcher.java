27ec7fa1be4ae3381c5703dbdf6c962870f3d524
h5rcode <h5rcode@gmail.com>
2017-09-17 17:26:21 +0200

    Google books searcher: download thumbnails.

--- app/src/main/java/com/blackbooks/services/search/google/GoogleBooksSearcher.java
+++ app/src/main/java/com/blackbooks/services/search/google/GoogleBooksSearcher.java
@@ -1,7 +1,10 @@
 package com.blackbooks.services.search.google;
 
+import android.graphics.Bitmap;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.RequestFuture;
 import com.blackbooks.services.search.BookOnlineSearchResult;
@@ -10,6 +13,7 @@ import com.blackbooks.services.search.BookSearcher;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -40,9 +44,32 @@ public final class GoogleBooksSearcher implements BookSearcher {
 
         try {
             JSONObject jsonObject = future.get();
-            return new GoogleBooksJSONParser().parse(jsonObject);
+            GoogleBook googleBook = new GoogleBooksJSONParser().parse(jsonObject);
+
+            if (googleBook.smallThumbnailLink != null) {
+                googleBook.smallThumbnail = downloadBitmap(googleBook.smallThumbnailLink);
+            }
+
+            if (googleBook.thumbnailLink != null) {
+                googleBook.thumbnail = downloadBitmap(googleBook.thumbnailLink);
+            }
+
+            return googleBook;
         } catch (ExecutionException | JSONException | InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private byte[] downloadBitmap(String bitmapUrl) throws InterruptedException, ExecutionException {
+        RequestFuture<Bitmap> futureBitmap = RequestFuture.newFuture();
+        ImageRequest imageRequest = new ImageRequest(bitmapUrl, futureBitmap, 0, 0, null, null, futureBitmap);
+
+        _requestQueue.add(imageRequest);
+
+        Bitmap bitmap = futureBitmap.get();
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        return stream.toByteArray();
     }
 }
